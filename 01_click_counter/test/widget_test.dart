90701baf13709f2ab000cfce23544b940ec3392b
divyanshkumarindia <divyanshkumarindia@gmail.com>
2025-10-11 14:55:23 +0530

    Modularized main.dart and moved Saved/Settings screens to separate files

--- 01_click_counter/test/widget_test.dart
+++ 01_click_counter/test/widget_test.dart
@@ -5,9 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:my_app/main.dart';
 
 void main() {
@@ -22,10 +20,10 @@ void main() {
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-  // Tap the 'Increase Number' button and wait for any animations to finish.
-  await tester.tap(find.text('Increase Number'));
-  // pumpAndSettle will run frames until all animations complete (or timeout).
-  await tester.pumpAndSettle(const Duration(milliseconds: 500));
+    // Tap the 'Increase Number' button and wait for any animations to finish.
+    await tester.tap(find.text('Increase Number'));
+    // pumpAndSettle will run frames until all animations complete (or timeout).
+    await tester.pumpAndSettle(const Duration(milliseconds: 500));
 
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
